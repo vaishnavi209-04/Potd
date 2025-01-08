d645c451603b9f21dc898cab8eec6bfe046e996b
Vaishnavi Mishra <vaishnavimishra986@gmail.com>
2025-01-08 18:47:35 +0530

    Count Prefix and Suffix Pairs I

---
+++ 8JAN2025/8JAN2025.java
@@ -0,0 +1,16 @@
+//Approach 1-Brute Force
+class Solution {
+    public int countPrefixSuffixPairs(String[] words) {
+        int n=words.length;
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(words[j].startsWith(words[i]) && words[j].endsWith(words[i]))
+                count++;
+            }
+        }
+        return count;
+    }
+}
