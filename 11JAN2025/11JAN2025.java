fe7c9b309ddf7331a6723daec517453bd5a4f48a
Vaishnavi Mishra <vaishnavimishra986@gmail.com>
2025-01-11 16:55:41 +0530

    Construct K Palindrome Strings

--- 11JAN2025/11JAN2025.java
+++ 11JAN2025/11JAN2025.java
@@ -1,1 +1,22 @@
-//check
+class Solution {
+    public boolean canConstruct(String s, int k) {
+        if(s.length()==k)
+        return true;
+        if(s.length()<k)
+        return false;
+        int[] arr=new int[26];
+        for(char ch:s.toCharArray())
+        {
+            arr[ch-'a']++;
+        }
+        int count=0;
+        for(int num:arr)
+        {
+            if(num%2==1)//count of odd characters
+            count++;
+        }
+        if(count>k)//because every odd count character needs a separate palindrome so n odd characters should atleast have n palindromes 
+        return false;
+        return true;
+    }
+}
