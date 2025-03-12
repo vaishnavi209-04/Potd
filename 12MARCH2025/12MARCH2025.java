ec651c749f59afe08bc2af24ab4fd8ea7858f6f9
Vaishnavi Mishra <vaishnavimishra986@gmail.com>
2025-03-12 15:46:08 +0530

    Maximum Count of Positive Integer and Negative Integer

---
+++ 12MARCH2025/12MARCH2025.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maximumCount(int[] nums) {
+        int pos=0,neg=0;
+        for(int num:nums)
+        {
+            if(num>0) pos++;
+            if(num<0) neg++;
+        }
+      return Math.max(pos,neg);
+    }
+}
